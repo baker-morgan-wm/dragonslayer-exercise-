94db93f0bb2c61f76c20a6e2ccbba863dcfe470d
Start Coding Student 2 <session2@START-Coding-MBP-C02PPE92G8WN.local>
2015-09-28 18:03:32 -0700

    I added a prompt to find the players name and an alert to respond to the players name as well as tell about the game

--- dragonslayer.js
+++ dragonslayer.js
@@ -5,6 +5,8 @@ var slaying = true
 var youHit =Math.floor(Math.random() * 2)
 var damageThisRound = Math.floor(Math.random()*5 + 1)
 var totalDamage = 0
+var name = prompt("Your name?");
+alert( name + " ,you today  will do battle with the monstrous Donald Trump. Pray the gods will down on you with favor. ");
 while(slaying) {
     if(youHit){
         console.log("You hit!");
